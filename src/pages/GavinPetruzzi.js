44072d348af5f5c1dbf4847fa540f17adf185c70
Gavin Petruzzi <gape5988@colorado.edu>
2023-11-16 20:45:41 -0700

    Gavin's page new

--- src/pages/GavinPetruzzi.js
+++ src/pages/GavinPetruzzi.js
@@ -10,8 +10,8 @@ export default function GavinPetruzziPage() {
           </div>
           <img
             className='pic'
-            src='https://attachments.office.net/owa/Gavin.Petruzzi%40colorado.edu/service.svc/s/GetAttachmentThumbnail?id=AAMkADQ3YWI4OTVkLWNhOGUtNDAwNi04ZTQ0LWMxZjJkNGVkMzg1MABGAAAAAAD7OJF22dK%2FSYyIzuEAaEHfBwCnik9GZerRS7cPKK7545CLAAAAAAEMAACnik9GZerRS7cPKK7545CLAACIvwYGAAABEgAQAGGwCPdKIWhOgtwm1LOCxyM%3D&thumbnailType=2&token=eyJhbGciOiJSUzI1NiIsImtpZCI6IjczRkI5QkJFRjYzNjc4RDRGN0U4NEI0NDBCQUJCMTJBMzM5RDlGOTgiLCJ0eXAiOiJKV1QiLCJ4NXQiOiJjX3VidnZZMmVOVDM2RXRFQzZ1eEtqT2RuNWcifQ.eyJvcmlnaW4iOiJodHRwczovL291dGxvb2sub2ZmaWNlLmNvbSIsInVjIjoiMGUyOGNmMGVmODUwNDdkMDk0MGZiMWYzYjQzMjA3YTUiLCJzaWduaW5fc3RhdGUiOiJbXCJrbXNpXCJdIiwidmVyIjoiRXhjaGFuZ2UuQ2FsbGJhY2suVjEiLCJhcHBjdHhzZW5kZXIiOiJPd2FEb3dubG9hZEAzZGVkOGIxYi0wNzBkLTQ2MjktODJlNC1jMGIwMTlmNDYwNTciLCJpc3NyaW5nIjoiV1ciLCJhcHBjdHgiOiJ7XCJtc2V4Y2hwcm90XCI6XCJvd2FcIixcInB1aWRcIjpcIjExNTM4MDExMjUwNjE3MTc0MjhcIixcInNjb3BlXCI6XCJPd2FEb3dubG9hZFwiLFwib2lkXCI6XCI1ZWI5MTdkNi0xNjQ5LTRlNGEtOGM2Ni1jNWQxMjEzODEzN2RcIixcInByaW1hcnlzaWRcIjpcIlMtMS01LTIxLTI0MTIxMTk1MDUtMzU2MDg5MjA1Ny0xNTAwNDI2Mjk0LTcwNzUzOTgxXCJ9IiwibmJmIjoxNzAwMDI2NTk1LCJleHAiOjE3MDAwMjcxOTUsImlzcyI6IjAwMDAwMDAyLTAwMDAtMGZmMS1jZTAwLTAwMDAwMDAwMDAwMEAzZGVkOGIxYi0wNzBkLTQ2MjktODJlNC1jMGIwMTlmNDYwNTciLCJhdWQiOiIwMDAwMDAwMi0wMDAwLTBmZjEtY2UwMC0wMDAwMDAwMDAwMDAvYXR0YWNobWVudHMub2ZmaWNlLm5ldEAzZGVkOGIxYi0wNzBkLTQ2MjktODJlNC1jMGIwMTlmNDYwNTciLCJoYXBwIjoib3dhIn0.IUM6tmtTrj-R-fvH26YT04nLRVjvxS-mpwH8bfk4QQfMbGiI025bd5EfFTquta0khk6m9S23-lEKUj-t29LfWgSvjHFO1kqZyZj_LIzwXdiWJbZtVQ-Pt_u2OM_wbqwtVMYuH1UqxubBi0CnArMoL6KE7eWgByEBg2GCCFsgo6Udm2pCO-pyRCfOFBiBr_3QIU3GWSpXR7gLnsIu8zlotqd3JIXG-uzmvuM0A8rtz6Gwecm_HTDiM2xG_e3w5vGEZgkB8BDeQ2fMO3A7G68tUZcE0iWr9UE8EjJjtVTvNr4PwFdDqMVoc57jwZQUH4QhWC56TYd13npTKf9ScYppjQ&X-OWA-CANARY=6Hiq5cTra0SpiQva9At64UD8hNac5dsYWARvA60UmKd6f9AM82McDQp5T_zIZWkDjc1SPNL9uOU.&owa=outlook.office.com&scriptVer=20231103003.19&animation=true'
-            alt='tom holland'
+            src='https://static.hudl.com/users/prod/8404198_fbe45d65e07048fa8bb9b420bb7c67de.jpg'
+            alt='gavin petruzzi'
           />
 
           <p className='description'>
@@ -65,8 +65,8 @@ export default function GavinPetruzziPage() {
           }
 
           .pic {
-            height: 450px;
-            width: 450px;
+            height: 500px;
+            width: 500px;
           }
 
           .button {
